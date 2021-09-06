c2fcaacdfe3d35caaa1c22ad16e800999b4cf1e6
iqbal-sk <mahammadiqbal684@gmail.com>
2021-09-06 13:46:28 +0530

    added selecting cells(multiple cells as well) and styling for content of cells

--- script.js
+++ script.js
@@ -44,10 +44,110 @@ $(document).ready(function() {
         $(this).toggleClass("selected");
     });
 
-    $(".input-cell").click(function() {
-        $(".input-cell.selected").removeClass("selected");
+    $(".input-cell").click(function(e) {
+        if(e.ctrlKey) {
+            let [rowId, colId] = getRowCol(this);
+            
+            if(rowId > 1) {
+                let topCellSelected = $(`#row-${rowId-1}-col-${colId}`).hasClass("selected");
+                if(topCellSelected) {
+                    $(this).addClass("top-cell-selected");
+                    $(`#row-${rowId-1}-col-${colId}`).addClass("bottom-cell-selected");
+                }
+            }
+
+            if(rowId < 100) {
+                let bottomCellSelected = $(`#row-${rowId+1}-col-${colId}`).hasClass("selected");
+                if(bottomCellSelected) {
+                    $(this).addClass("bottom-cell-selected");
+                    $(`#row-${rowId+1}-col-${colId}`).addClass("top-cell-selected");
+                }
+            }
+
+            if(colId > 1) {
+                let leftCellSelected = $(`#row-${rowId}-col-${colId-1}`).hasClass("selected");
+                if(leftCellSelected) {
+                    $(this).addClass("left-cell-selected");
+                    $(`#row-${rowId}-col-${colId-1}`).addClass("right-cell-selected");
+                }
+            }
+
+            if(colId < 100) {
+                let rightCellSelected = $(`#row-${rowId}-col-${colId+1}`).hasClass("selected");
+                if(rightCellSelected) {
+                    $(this).addClass("right-cell-selected");
+                    $(`#row-${rowId}-col-${colId+1}`).addClass("left-cell-selected");
+                }
+            }
+        }
+        else {
+            // console.log($(".input-cell.selected")[0]);
+            // TODO: remove class "top-cell-selected", "bottom-cell-selected", "left-cell-selected", "right-cell-selected" all the cells that are selected with cytrl key
+            $(".input-cell.selected").removeClass("selected");
+            $(".input-cell.top-cell-selected").removeClass("top-cell-selected");
+            $(".input-cell.bottom-cell-selected").removeClass("bottom-cell-selected");
+            $(".input-cell.left-cell-selected").removeClass("left-cell-selected");
+            $(".input-cell.right-cell-selected").removeClass("right-cell-selected");
+        }
+
         $(this).addClass("selected");
     });
+
+    $(".input-cell").dblclick(function() {
+        $(".input-cell.selected").removeClass("selected");
+        $(this).addClass("selected");
+        $(this).attr("contenteditable", "true");
+        $(this).focus();
+    });
+
+    $(".input-cell").blur(function() {
+        $(".input-cell.selected").attr("contenteditable", "false");
+    })
+
+    $(".input-cell-container").scroll(function () {
+        $(".column-name-container").scrollLeft(this.scrollLeft);
+        $(".row-name-container").scrollTop(this.scrollTop);
+    });
+    
+
+    function getRowCol(ele) {
+        let idArray = $(ele).attr("id").split("-");
+        let rowId = parseInt(idArray[1]);
+        let colId = parseInt(idArray[3]);
+
+        return [rowId, colId];
+    }
+    
+    function updateCell(property, value) {
+        $(".input-cell.selected").each(function() {
+            $(this).css(property, value);
+        });
+    }
+
+    $(".icon-bold").click(function() {
+        if($(this).hasClass("selected")) {
+            updateCell("font-weight", "");
+        } else {
+            updateCell("font-weight", "bold");
+        }
+    });
+
+    $(".icon-italic").click(function() {
+        if($(this).hasClass("selected")) {
+            updateCell("font-style","");
+        } else {
+            updateCell("font-style","italic");
+        }
+    });
+    
+    $(".icon-underline").click(function() {
+        if($(this).hasClass("selected")) {
+            updateCell("text-decoration","");
+        } else {
+            updateCell("text-decoration","underline");
+        }
+    });
+    
 
 });
 
